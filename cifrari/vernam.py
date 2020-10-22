71df8ac7a39d0954d4c429dff538ee78d2051424
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-10-22 09:42:47 +0200

    vernam

---
+++ cifrari/vernam.py
@@ -0,0 +1,21 @@
+
+a = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+toa = {k: i for i, k in enumerate(a)}
+
+
+def encrypt(msg, key):
+    assert len(msg) <= len(key)
+    return [toa[cm] + toa[ck] for cm, ck in zip(msg, key)]
+
+
+def decrypt(msg, key):
+    assert len(msg) <= len(key)
+    return ''.join([
+        a[(cm - ck) % len(a)] for cm, ck in zip(msg, map(lambda k: toa[k], key))
+    ])
+
+
+if __name__ == "__main__":
+    MSG = 'abc'
+    KEY = 'keylong'
+    assert MSG == decrypt(encrypt(MSG, KEY), KEY)
