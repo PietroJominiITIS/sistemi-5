c70aacbf23e1bfb76e89d8b6b92685ad611155c0
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-11-19 20:54:29 +0100

    chunk != string, duh

--- cifrari/rsa/test.py
+++ cifrari/rsa/test.py
@@ -24,12 +24,13 @@ from rsa import keygen, encrypt_packed, decrypt_packed, encrypt_array, decrypt_a
 from time import time
 from sys import argv
 
-def test_array(priv, pub):
-    target = [0, 1, 1, 2,  3,  5,  8, 13, 21, 34, 55, 89, 144]
+target = 'big nose :==), unicode certified こんにちは'
+btarget = bytearray(target.encode())
 
+def test_array(priv, pub):
     start = time()
     print('\tEncryption...', end='')
-    encrypted = encrypt_array(target, pub)
+    encrypted = encrypt_array(btarget, pub)
     print(f'\r\tEncrypted in {round(time() - start, 4)}')
 
     start = time()
@@ -37,12 +38,9 @@ def test_array(priv, pub):
     decrypted = decrypt_array(encrypted, priv)    
     print(f'\r\tDecripted in {round(time() - start, 4)}')
 
-    print('\tOK' if list(decrypted) == target else 'FAILED')
+    print('\tOK' if bytearray(decrypted).decode() == target else 'FAILED')
 
-def test_string(priv, pub):
-    target = 'big nose :==), unicode certified こんにちは'
-    btarget = bytearray(target.encode())
-
+def test_chunk(priv, pub):
     start = time()
     print('\tEncryption...', end='')
     encrypted = encrypt_packed(btarget, pub)
@@ -65,8 +63,8 @@ def test_all():
     print('Testing array')
     test_array(priv, pub)
 
-    print('Testing string')
-    test_string(priv, pub)
+    print('Testing chunk')
+    test_chunk(priv, pub)
 
 if __name__ == "__main__":
     test_all()
